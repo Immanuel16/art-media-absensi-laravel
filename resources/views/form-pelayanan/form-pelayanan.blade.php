9e7a514c845115c06315a3b551b4263697bf4d2c
Miduk Immanuel <v-miduk.immanuel@anteraja.id>
2021-12-25 10:58:21 +0700

    push

--- resources/views/form-pelayanan/form-pelayanan.blade.php
+++ resources/views/form-pelayanan/form-pelayanan.blade.php
@@ -25,7 +25,7 @@
                         </div>
                         <div class="col-md-12 col-lg-12 col-sm-12 mt-3" align="center">
                             <p class="title-countdown-opened alert-notif mt-4">Times left :</p>
-                            <p class="countdown-opened mb-4" style="color:#f7e8e5;font-weight: 800 !important;margin-top:-10px">
+                            <p class="countdown-opened mb-4" style="color:#75594e;font-weight: 800 !important;margin-top:-10px">
                                 <span id="daysOpened" class="mr-2"></span> <span id="hoursOpened"
                                     class="mr-2"></span> <span id="minutesOpened" class="mr-2"></span>
                                 <span id="secondsOpened" class="mr-2"></span>
