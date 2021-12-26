4f67b2f334dde9f6e28711a3d817b976304824ec
Miduk Immanuel <v-miduk.immanuel@anteraja.id>
2021-12-26 10:47:30 +0700

    fixing table name

--- app/Http/Controllers/DataPelayananController.php
+++ app/Http/Controllers/DataPelayananController.php
@@ -15,7 +15,7 @@ class DataPelayananController extends Controller
 
     public function index()
     {
-        $crews = DB::table('data-pelayanan')->where('tanggal', 'like', '%Januari%')->distinct('fullname')->paginate(10);
+        $crews = DB::table('data_pelayanan')->where('tanggal', 'like', '%Januari%')->distinct('fullname')->paginate(10);
 
         return view('data-pelayanan.data-pelayanan', ['crews' => $crews]);
     }
@@ -24,7 +24,7 @@ class DataPelayananController extends Controller
     {
         $cari = $req->cari;
 
-        $crews = DB::table('data-pelayanan')->where('tanggal', 'like', '%Januari%')->distinct('fullname')->where(function ($query) use ($cari) {
+        $crews = DB::table('data_pelayanan')->where('tanggal', 'like', '%Januari%')->distinct('fullname')->where(function ($query) use ($cari) {
             $query->where('tanggal', 'like', '%' . $cari . '%')->orWhere('fullname', 'like', '%' . strtolower($cari) . '%');
         })->paginate(15);
 
