7cbde536951a8245e0d78ec8868926acf22a6052
Miduk Immanuel <v-miduk.immanuel@anteraja.id>
2021-12-26 12:45:47 +0700

    test

--- resources/views/data-pelayanan/data-pelayanan.blade.php
+++ resources/views/data-pelayanan/data-pelayanan.blade.php
@@ -43,18 +43,18 @@
                 </div>
             </div>
             <br>
-            <div class="row mt-3">
-                <div class="col-md-12" align="right">
-                    <div class="" style="display:flex;justify-content: center">
-                        {{ $crews->links() }}
-                    </div>
-                </div>
+            <div class="d-flex justify-content-center">
+                {{ $crews->links() }}
             </div>
-            <div class="row mt-1">
+            {{-- <div class="row mt-3">
+                <div class="col-md-12" align="right">
+                </div>
+            </div> --}}
+            {{-- <div class="row mt-1">
                 <div class="col-md-12" align="center">
                     Halaman <span style="font-weight: 600">{{ $crews->currentPage() }}</span> dari <span style="font-weight: 600">{{ $crews->lastPage() }}</span>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 @endsection
