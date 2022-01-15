880e1532393b51a35721083ac1f39565e3067f1e
Miduk Immanuel <miduk.immanuel@ukur.com>
2022-01-15 16:08:21 +0700

    fixing date opened 3

--- app/Http/Controllers/FormPelayananController.php
+++ app/Http/Controllers/FormPelayananController.php
@@ -41,7 +41,7 @@ class FormPelayananController extends Controller
         $fullname = Request()->fullname;
         $email = DB::table('data_email')->select('email')->where('fullname', 'like', Request()->fullname);
 
-        dd($email)
+        dd($email);
         
         $tgl = implode('; ', Request()->tanggal);
         $tglExplode = explode("; ", $tgl);
